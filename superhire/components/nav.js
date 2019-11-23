f8ab4d0aeb335b6b0a2afa1615ded2bb99ba5c09
Joshua Armah <developer@joshuaarmah.com>
2019-11-23 14:28:30 -0800

    Added Algolia, and Firebase SDK

--- superhire/components/nav.js
+++ superhire/components/nav.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
+  { href: 'https://zeit.co/now', label: 'Diversely' },
   { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
@@ -14,7 +14,7 @@ const Nav = () => (
     <ul>
       <li>
         <Link href="/">
-          <a>Home</a>
+          <a>Search</a>
         </Link>
       </li>
       {links.map(({ key, href, label }) => (
